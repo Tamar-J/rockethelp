3f5dd715a93b089587be7215e6fc48116bba483f
Tamar-J <tamar_judson@hotmail.com>
2022-07-21 21:39:47 -0300

    antes de atualizar

--- src/@types/navigation.d.ts
+++ src/@types/navigation.d.ts
@@ -3,6 +3,7 @@ export declare global {
     interface RootParamList {
       home: undefined
       new: undefined
+      signup: undefined
       details: { orderId: string}
     }
   }
--- src/screens/SignUp.tsx
+++ src/screens/SignUp.tsx
@@ -7,49 +7,28 @@ import auth from '@react-native-firebase/auth'
 import Logo from '../assets/logo_primary.svg'
 import { Button } from '../components/Button'
 import { Input } from '../components/Input'
-import { useNavigation } from '@react-navigation/native'
 
-export default function SignIn() {
+export function SignUp() {
   const [isLoading, setIsLoading] = useState(false)
   const [email, setEmail] = useState('')
   const [password, setPassword] = useState('')
 
-  const navigation = useNavigation()
   const { colors } = useTheme()
 
-  const handleCreateAccount = () => {
-    navigation.navigate('signup')
-  }
-
-  const handleSignIn = () => {
+  const handleSignUp = () => {
     if (!email || !password) {
-      return Alert.alert('Entrar', 'Informe e-mail e senha')
+      return Alert.alert('Registrar', 'Informe e-mail e senha')
     }
     
     setIsLoading(true)
     auth()
-    .signInWithEmailAndPassword(email, password)
-    .then(response => {
-      //console.log(response)
+    .createUserWithEmailAndPassword(email, password)
+    .then(newUser => {
+      console.log(newUser)
     })
     .catch(error => {
       console.log(error)
       setIsLoading(false)
-
-      if (error.code === 'auth/invalid-email') {
-        return Alert.alert('Entrar', 'E-mail inválido.')
-      }
-
-      if (error.code === 'auth/wrong-password') {
-        return Alert.alert('Entrar', 'E-mail ou senha inválida.')
-      }      
-      
-      if (error.code === 'auth/user-not-found') {
-        return Alert.alert('Entrar', 'Usuário não cadastrado.')
-      }
-
-      return Alert.alert('Entrar', 'Algo deu errado.')
-
     })
   }
 
@@ -57,7 +36,7 @@ export default function SignIn() {
     <VStack flex={1} alignItems='center' bg='gray.600' px={8} pt={24}>
       <Logo />
       <Heading color='gray.100' fontSize='xl' mt={20} mb={6}>
-        Acesse sua conta
+        Crie sua conta
       </Heading>
 
       <Input 
@@ -66,6 +45,15 @@ export default function SignIn() {
         InputLeftElement={<Icon as={<Envelope color={colors.gray[300]}/>} ml={4} />}
         mb={4}
       />
+
+      <Input 
+        placeholder='Senha' 
+        onChangeText={setPassword}
+        InputLeftElement={<Icon as={<Key color={colors.gray[300]}/>} ml={4} />}
+        mb={8}
+        secureTextEntry
+      />
+
       <Input 
         placeholder='Senha' 
         onChangeText={setPassword}
@@ -75,17 +63,10 @@ export default function SignIn() {
       />
 
       <Button 
-        title="Entrar" 
+        title="Registar" 
         w='full' 
-        onPress={handleSignIn}
+        onPress={handleSignUp}
         isLoading={isLoading}
-      />
-
-      <Button 
-        title="Criar Conta" 
-        w='full' 
-        onPress={handleCreateAccount}
-        //isLoading={isLoading}
       />
     </VStack>
   )
