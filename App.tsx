7d6c3a57fca116c65857ba6596c8540a16932362
Tamar-J <tamar_judson@hotmail.com>
2022-07-25 18:18:01 -0300

    fix: styles/theme.ts path

--- App.tsx
+++ App.tsx
@@ -2,7 +2,7 @@ import { NativeBaseProvider } from 'native-base'
 import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto'
 
 import SignIn from './src/screens/SignIn'
-import { THEME } from './src/screens/styles/theme'
+import { THEME } from './src/styles/theme'
 import { Loading } from './src/components/Loading'
 
 export default function App() {
@@ -12,7 +12,7 @@ const [fontsLoaded] = useFonts({
 })
   return (
     <NativeBaseProvider theme={THEME}>
-      <Loading />
+      <SignIn />
     </NativeBaseProvider>
   )
 }
