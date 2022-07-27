192665f7f1a4b23c42125b115ecb31f8401b551e
Tamar-J <tamar_judson@hotmail.com>
2022-07-27 19:28:24 -0300

    feat: add Input.tsx component

---
+++ src/components/Input.tsx
@@ -0,0 +1,29 @@
+import { Input as NativeBaseInput, IInputProps } from 'native-base';
+
+export function Input({ ...rest }: IInputProps) {
+  return (
+    <NativeBaseInput
+      h={14}
+      rounded={6}
+      borderWidth={0}
+      placeholderTextColor='gray.300'
+      backgroundColor='gray.700'
+      color='white'
+      fontSize='md'
+      fontFamily='body'
+      autoCapitalize='none'
+      _focus={{
+        borderWidth: 1,
+        borderColor: '#FFBC70'
+      }}
+      {...rest}
+    />
+    
+  );
+}
+
+/* 
+<VStack w='full' pl={4} mb={1} backgroundColor='red'>
+  <Text color='#F75A68' fontSize='xs'>E-mail inválido</Text>
+</VStack> 
+*/
--- src/screens/SignIn.tsx
+++ src/screens/SignIn.tsx
@@ -1,8 +1,9 @@
-import { VStack, Heading, Input, Button, Text, HStack, Icon, Pressable, useTheme, KeyboardAvoidingView, ScrollView as Container } from 'native-base'
+import { VStack, Heading, Button, Text, HStack, Icon, Pressable, useTheme, KeyboardAvoidingView, ScrollView as Container } from 'native-base'
 import { Envelope, Eye, EyeClosed, Key } from 'phosphor-react-native';
 import { Platform } from 'react-native';
 
 import Logo from "../assets/logo_primary.svg";
+import { Input } from '../components/Input';
 
 export default function SignIn() {
   const { colors } = useTheme()
@@ -21,47 +22,18 @@ export default function SignIn() {
           <Input
             InputLeftElement={<Icon as={<Envelope color={colors.gray[300]}/>} ml={4} />}
             placeholder='E-mail'
-            placeholderTextColor='gray.300'
-            h={14}
-            rounded={6}
-            backgroundColor='gray.700'
-            color='white'
-            fontSize='md'
-            fontFamily='body'
-            autoCapitalize='none'
-            _focus={{
-              borderWidth: 1,
-              borderColor: '#FFBC70'
-            }}
             keyboardType='email-address'
-            borderWidth={0}
             mb={4}
           />
-          {/* <VStack w='full' pl={4} mb={1} backgroundColor='red'>
-            <Text color='#F75A68' fontSize='xs'>E-mail inválido</Text>
-          </VStack> */}
+
 
           <Input 
             InputLeftElement={<Icon as={<Key color={colors.gray[300]}/>} ml={4} />}
             InputRightElement={
-              <Icon as={<Eye color={colors.gray[300]}/>
-            } 
-            mr={4} />}
-            secureTextEntry
+              <Icon as={<Eye color={colors.gray[300]}/> } mr={4} />
+            }
             placeholder='Senha'
-            placeholderTextColor='gray.300'
-            backgroundColor='gray.700'
-            color='white'
-            h={14}
-            rounded={6}
-            fontSize='md'
-            fontFamily='body'
-            autoCapitalize='none'
-            _focus={{
-              borderWidth: 1,
-              borderColor: '#FFBC70'
-            }}
-            borderWidth={0}
+            secureTextEntry
             mb={6}
           />
 
