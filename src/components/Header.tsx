602f4a836b59a68f35a9672ed6c7b91ecac39041
Tamar-J <tamar_judson@hotmail.com>
2022-07-29 23:03:49 -0300

    feat: opcional header component title

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,8 +2,8 @@ import { Heading, HStack, IconButton, Text, useTheme, VStack } from 'native-base
 import { CaretLeft, IconProps } from 'phosphor-react-native';
 
 type HeaderProps = {
-  title: string
-  icon: React.ElementType<IconProps>
+  title?: 'EM ANDAMENTO' | 'FINALIZADO'
+  icon?: React.ElementType<IconProps>
 }
 
 export function Header({ title, icon: Icon }:HeaderProps) {
@@ -11,29 +11,32 @@ export function Header({ title, icon: Icon }:HeaderProps) {
 
   return (
     <VStack bg='gray.600'>
-      <HStack  mt={20} justifyContent='center' alignItems='center'>
+      <HStack  mt={20} justifyContent='center' alignItems='center' mb={6}>
         <IconButton
           icon={<CaretLeft color={colors.gray[200]} size={24} />}
           position='absolute'
           left={0}
           ml={4}
         />
-        <Heading color='gray.100' size='lg'>Solicitação</Heading>
+        <Heading color='gray.100' size='md'>Solicitação</Heading>
       </HStack>
-      <HStack 
-        alignItems='center' 
-        justifyContent='center' 
-        bg='gray.500' 
-        w='full' 
-        py={4}
-        mt={6}
-        rounded={5}
-      >
-        <Icon color={colors.green[300]}/>
-        <Text color='green.300' fontSize='sm' fontFamily='heading' ml={4}>
-          {title}
-        </Text>
-      </HStack>
+      {
+        title && Icon && (
+          <HStack 
+            alignItems='center' 
+            justifyContent='center' 
+            bg='gray.500' 
+            w='full' 
+            py={4}
+            rounded={5}
+          >
+            <Icon color={colors.green[300]}/>
+            <Text color='green.300' fontSize='sm' fontFamily='heading' ml={4}>
+              {title}
+            </Text>
+          </HStack>
+        )
+      }
     </VStack>
   )
 }
