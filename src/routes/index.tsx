5e3bb0d6567f5e9a0d55eea0fd756ad177dc032a
Tamar-J <tamar_judson@hotmail.com>
2022-08-02 19:15:46 -0300

    BREAKING CHANGE: add firebase, and lost expo go support

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,12 +1,12 @@
 import { NavigationContainer } from '@react-navigation/native'
 
-//import { AuthRoutes } from './auth.routes'
-import { AppRoutes } from './app.routes'
+import { AuthRoutes } from './auth.routes'
+//import { AppRoutes } from './app.routes'
 
 export function Routes() {
   return (
     <NavigationContainer>
-      <AppRoutes />
+      <AuthRoutes />
     </NavigationContainer>
   )
 }
--- src/screens/SignUp.tsx
+++ src/screens/SignUp.tsx
@@ -1,18 +1,47 @@
+import auth from '@react-native-firebase/auth';
 import { useNavigation } from '@react-navigation/native';
 import { VStack, Heading, Text, HStack, Icon, useTheme, KeyboardAvoidingView, ScrollView as Container } from 'native-base'
 import { Envelope, Key } from 'phosphor-react-native';
-import { Platform } from 'react-native';
+import { useState } from 'react';
+import { Alert, Platform } from 'react-native';
 
 import Logo from "../assets/logo_primary.svg";
 import { Button, TextButton } from '../components/Button';
 import { Input } from '../components/Input';
 
 export default function SignUp() {
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+  const [passwordConfirmation, setPasswordConfirmation] = useState('')
+  const [isLoading, setIsLoading] = useState(false)
+
   const { colors } = useTheme()
   const { goBack } = useNavigation()
 
   const handleSignIn = () => {
     goBack()
+  }
+
+  const handleSignUp = () => {
+    if (email === '' || password === '') {
+      return Alert.alert('Criar Conta', 'Informe e-mail e senha')
+    }
+    if (password !== passwordConfirmation) {
+      return Alert.alert('Criar Conta', 'Senhas diferentes')
+    }
+
+    setIsLoading(true)
+    
+    auth()
+      .createUserWithEmailAndPassword(email, password)
+      .then(response => console.log(response))
+      .catch(error => {
+        if (error.code === 'auth/invalid-email') {
+          Alert.alert('Criar Conta', 'E-mail inválido.')
+        }
+        console.log(error)
+        setIsLoading(false)
+      })
   }
 
   return (
@@ -31,6 +60,8 @@ export default function SignUp() {
             InputLeftElement={<Icon as={<Envelope color={colors.gray[300]}/>} ml={4} />}
             placeholder='E-mail'
             keyboardType='email-address'
+            value={email}
+            onChangeText={setEmail}
             mb={4}
           />
 
@@ -38,6 +69,8 @@ export default function SignUp() {
             InputLeftElement={<Icon as={<Key color={colors.gray[300]}/>} ml={4} />}
             placeholder='Senha'
             secureTextEntry
+            value={password}
+            onChangeText={setPassword}
             mb={4}
           />
 
@@ -45,10 +78,12 @@ export default function SignUp() {
             InputLeftElement={<Icon as={<Key color={colors.gray[300]}/>} ml={4} />}
             placeholder='Confirme a senha'
             secureTextEntry
+            value={passwordConfirmation}
+            onChangeText={setPasswordConfirmation}
             mb={6}
           />
 
-          <Button title='Registrar' mb={4} />
+          <Button title='Registrar' mb={4} onPress={handleSignUp} isLoading={isLoading}/>
 
           <HStack 
             w='full' 
