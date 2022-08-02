e950e1077e5c16ceb17fabd7094a7cde0a2c191f
Tamar-J <tamar_judson@hotmail.com>
2022-08-02 20:02:48 -0300

    feat: user authentication with firebase

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,12 +1,29 @@
+import auth, { FirebaseAuthTypes } from '@react-native-firebase/auth'
 import { NavigationContainer } from '@react-navigation/native'
+import { useEffect, useState } from 'react'
 
+import { Loading } from '../components/Loading'
 import { AuthRoutes } from './auth.routes'
-//import { AppRoutes } from './app.routes'
+import { AppRoutes } from './app.routes'
 
 export function Routes() {
+  const [isLoading, setIsLoading] = useState(true)
+  const [user, setUser] = useState<FirebaseAuthTypes.User>()
+  useEffect(() => {
+    const subscriber =  auth()
+      .onAuthStateChanged(response => {
+        setUser(response)
+        setIsLoading(false)
+      })
+  
+    return subscriber
+  }, [])
+
+  if (isLoading) return <Loading/>
+
   return (
     <NavigationContainer>
-      <AuthRoutes />
+      {user ? <AppRoutes /> : <AuthRoutes />}
     </NavigationContainer>
   )
 }
--- src/screens/SignIn.tsx
+++ src/screens/SignIn.tsx
@@ -1,19 +1,50 @@
+import auth from '@react-native-firebase/auth';
 import { useNavigation } from '@react-navigation/native';
 import { VStack, Heading, Text, HStack, Icon, useTheme, KeyboardAvoidingView, ScrollView as Container } from 'native-base'
 import { Envelope, Eye, EyeClosed, Key } from 'phosphor-react-native';
-import { Platform } from 'react-native';
+import { useState } from 'react';
+import { Alert, Platform } from 'react-native';
 
 import Logo from "../assets/logo_primary.svg";
 import { Button, TextButton } from '../components/Button';
 import { Input } from '../components/Input';
 
 export default function SignIn() {
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+  const [isLoading, setIsLoading] = useState(false)
+
   const { navigate } = useNavigation()
   const { colors } = useTheme()
 
   const handleLogin = () => {
+    if (email === '' || password === '') {
+      return Alert.alert('Entrar', 'Informe e-mail e senha')
+    }
+    setIsLoading(true)
+    auth()
+      .signInWithEmailAndPassword(email, password)
+      //.then(response => console.log(response))
+      .catch(error => {
+        console.log(error)
+        setIsLoading(false)
 
+        if (error.code === 'auth/invalid-email') {
+          return Alert.alert('Entrar', 'E-mail inválido.')
+        }
+  
+        if (error.code === 'auth/wrong-password') {
+          return Alert.alert('Entrar', 'E-mail ou senha inválida.')
+        }      
+        
+        if (error.code === 'auth/user-not-found') {
+          return Alert.alert('Entrar', 'Usuário não cadastrado.')
+        }
+  
+        return Alert.alert('Entrar', 'Algo deu errado.')
+      })
   }
+
   const handleSignUp = () => {
     navigate('SignUp')
   }
@@ -34,6 +65,8 @@ export default function SignIn() {
             InputLeftElement={<Icon as={<Envelope color={colors.gray[300]}/>} ml={4} />}
             placeholder='E-mail'
             keyboardType='email-address'
+            value={email}
+            onChangeText={setEmail}
             mb={4}
           />
 
@@ -43,11 +76,13 @@ export default function SignIn() {
               <Icon as={<Eye color={colors.gray[300]}/> } mr={4} />
             }
             placeholder='Senha'
+            value={password}
+            onChangeText={setPassword}
             secureTextEntry
             mb={6}
           />
 
-          <Button title='Entrar' mb={4} onPress={handleLogin}/>
+          <Button title='Entrar' mb={4} onPress={handleLogin} isLoading={isLoading}/>
 
           <HStack 
             w='full' 
