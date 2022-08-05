437c26a6a2d037d51f3161dbe8683b11723cb610
Tamar-J <tamar_judson@hotmail.com>
2022-08-05 00:32:42 -0300

    feat: add transition animation between screens

--- src/routes/app.routes.tsx
+++ src/routes/app.routes.tsx
@@ -1,4 +1,5 @@
 import { createStackNavigator } from '@react-navigation/stack'
+import { Animated } from 'react-native'
 
 import { Details } from '../screens/Details'
 import { Home } from '../screens/Home'
@@ -6,13 +7,31 @@ import { Register } from '../screens/Register'
 
 const { Navigator, Screen } = createStackNavigator()
 
+type TransitionSpec = {
+  animation: 'spring' | 'timing'
+  config: Omit<Animated.SpringAnimationConfig, 'toValue' | keyof Animated.AnimationConfig>
+}
+
 export function AppRoutes() {
+  const config: TransitionSpec = {
+    animation: 'spring',
+    config: {
+      stiffness: 1000,
+      damping: 500,
+      mass: 3,
+      overshootClamping: true,
+      restDisplacementThreshold: 0.01,
+      restSpeedThreshold: 0.01
+    }
+  }
+
   return (
     <Navigator 
       screenOptions={{
         headerShown: false,
         gestureEnabled: true,
-        gestureDirection: 'horizontal'
+        gestureDirection: 'horizontal',
+        transitionSpec: { open: config, close: config }
       }}
     >
       <Screen name='Home' component={Home} />
